3ffee6ff6bf85035320a94227b193dc50f721133
clement <clement@portto.com>
2023-01-07 00:16:53 +0800

    chore: update wallet icon

--- app/src/main/java/com/example/walletconnectsample/ui/proposal/SessionProposalFragment.kt
+++ app/src/main/java/com/example/walletconnectsample/ui/proposal/SessionProposalFragment.kt
@@ -1,6 +1,5 @@
 package com.example.walletconnectsample.ui.proposal
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/com/example/walletconnectsample/utils/Constants.kt
+++ app/src/main/java/com/example/walletconnectsample/utils/Constants.kt
@@ -1,8 +1,7 @@
 package com.example.walletconnectsample.utils
 
 const val relayUrl = "relay.walletconnect.com"
-const val walletIconUrl =
-    "https://gblobscdn.gitbook.com/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media"
+const val walletIconUrl = "https://avatars.githubusercontent.com/u/6955922?s=280&v=4"
 
 
 // Navigation args
